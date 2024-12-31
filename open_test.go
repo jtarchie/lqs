8a34e6c2a6fcdb19641f17c4a792fdfec5006aaa
JT Archie <jtarchie@gmail.com>
2024-12-31 14:47:05 -0700

    add more tests

--- open_test.go
+++ open_test.go
@@ -2,6 +2,9 @@ package lqs_test
 
 import (
 	"context"
+	"os"
+	"runtime"
+	"sync"
 	"testing"
 
 	"github.com/alecthomas/assert/v2"
@@ -10,7 +13,10 @@ import (
 )
 
 func TestPragma(t *testing.T) {
-	client, err := lqs.Open("sqlite3", ":memory:", "PRAGMA cache_size = 1234;")
+	tempFile, err := os.CreateTemp("", "lqs_test")
+	assert.NoError(t, err)
+
+	client, err := lqs.Open("sqlite3", "file://"+tempFile.Name(), "PRAGMA cache_size = 1234;")
 	assert.NoError(t, err)
 	defer client.Close()
 
@@ -30,6 +36,55 @@ func TestPragma(t *testing.T) {
 	err = rows.Scan(&cache_size)
 	assert.NoError(t, err)
 	assert.Equal(t, cache_size, 1234)
+}
+
+func TestThreadSafe(t *testing.T) {
+	tempFile, err := os.CreateTemp("", "lqs_test")
+	assert.NoError(t, err)
+
+	client, err := lqs.Open("sqlite3", "file://"+tempFile.Name(), "PRAGMA cache_size = 1234;")
+	assert.NoError(t, err)
+	defer client.Close()
+
+	client.SetMaxOpenConns(runtime.NumCPU())
+	client.SetMaxIdleConns(runtime.NumCPU())
+
+	var wg sync.WaitGroup
+
+	for range runtime.NumCPU() {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			var cache_size int64
+
+			err := client.QueryRow(`PRAGMA cache_size;`).Scan(&cache_size)
+			assert.NoError(t, err)
+			assert.Equal(t, cache_size, 1234)
+
+			rows, err := client.Query("PRAGMA cache_size;")
+			assert.NoError(t, err)
+			defer rows.Close()
+
+			cache_size = 0
+			rows.Next()
+			err = rows.Scan(&cache_size)
+			assert.NoError(t, err)
+			assert.Equal(t, cache_size, 1234)
+		}()
+	}
+	wg.Wait()
+}
+
+func TestForeignKeys(t *testing.T) {
+	client, err := lqs.Open("sqlite3", ":memory:", "PRAGMA foreign_keys = ON;")
+	assert.NoError(t, err)
+	defer client.Close()
+
+	var foreign_keys int64
+	err = client.QueryRow(`PRAGMA foreign_keys;`).Scan(&foreign_keys)
+	assert.NoError(t, err)
+	assert.Equal(t, foreign_keys, 1)
 }
 
 // ensure errors are propagated correctly
